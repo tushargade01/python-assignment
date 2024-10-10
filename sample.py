a549fcd2f9bab066ef11f9247e1b048d4546cdb9
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-10-10 21:30:52 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,11 @@
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players)
+# print(players[0:5:2])
+
+temp = {
+    'tushar':'python',
+    'sachin':'rect',
+    'balaji':'.net'
+}
+#print(temp['rohan'])
+print(temp.get('rohan','rohan is not present in list'))
---
+++ teach/listProgram.py
@@ -0,0 +1,5 @@
+myList = [1,5,6,3]
+print(myList)
+myList.append(9)
+myList.index(0,2)
+myList.remove(0)
